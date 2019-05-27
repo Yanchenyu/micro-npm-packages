d3968aef90095d4d4b90728459fb627440f4f35a
yan_cy <yan_cy@Ctrip.com>
2019-05-27 12:45:46 +0800

    flatten函数实现

--- src/flatten/index.js
+++ src/flatten/index.js
@@ -1,11 +1,32 @@
+/**
+ * @description 数组扁平化（支持无限嵌套）
+ * @author yancy
+ * @example 
+ * var arr = [1,2,[3,4,[5,[6]]],7,8,[9,10,[11]]];
+ * flattenDeep(arr);    // [1,2,3,4,5,6,7,8,9,10,11];
+ */
+
+
+var arr = [1,2,[3,4,[5,[6]]],7,8,[9,10,[11]]];
+
+
+// 实现一：传统递归形式
+
 var dataType = require('../type/index.js');
 
-function flatten(arr) {
-    
-    // if (dataType.isArray(arr)) {
-    //     flatten(arr)
-    // }
-    return dataType(arr)
+function flattenDeep(arr) {
+    if (!dataType.isArray(arr)) return
+    var newArr = [];
+    for (var i = 0,l = arr.length; i < l; i++) {
+        if (dataType.isArray(arr[i])) {
+            newArr = newArr.concat(flattenDeep(arr[i]));
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr
 }
 
-console.log(flatten([1,2,3]))
+console.log(flattenDeep(arr))
+
+// 实现二：
